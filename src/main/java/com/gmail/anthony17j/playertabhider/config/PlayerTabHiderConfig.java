86844fa4e9a9e6564922db8aae55676bd31190ae
Uxzylon <anthony17j@gmail.com>
2022-01-19 21:04:53 +0100

    Changed config

--- src/main/java/com/gmail/anthony17j/playertabhider/config/PlayerTabHiderConfig.java
+++ src/main/java/com/gmail/anthony17j/playertabhider/config/PlayerTabHiderConfig.java
@@ -16,19 +16,13 @@ public class PlayerTabHiderConfig {
 
     public static final Path path = FabricLoader.getInstance().getConfigDir().resolve("playertabhider.json");
 
-    public static final PropertyMirror<Boolean> hideTabEnabled = PropertyMirror.create(ConfigTypes.BOOLEAN);
-    public static final PropertyMirror<Boolean> hideCountEnabled = PropertyMirror.create(ConfigTypes.BOOLEAN);
-    public static final PropertyMirror<String[]> playerListList = PropertyMirror.create(ConfigTypes.makeArray(ConfigTypes.STRING));
-    public static final PropertyMirror<String[]> playerCountList = PropertyMirror.create(ConfigTypes.makeArray(ConfigTypes.STRING));
+    public static final PropertyMirror<Boolean> hideEnabled = PropertyMirror.create(ConfigTypes.BOOLEAN);
+    public static final PropertyMirror<String[]> playerList = PropertyMirror.create(ConfigTypes.makeArray(ConfigTypes.STRING));
 
     public static final ConfigTree tree = ConfigTree.builder()
-            .fork("hide_from_player_list")
-            .withMirroredValue("enabled", hideTabEnabled, false)
-            .withMirroredValue("value", playerListList, new String[] { "Player1", "Player2" })
-            .finishBranch()
-            .fork("hide_from_player_count")
-            .withMirroredValue("enabled", hideCountEnabled, false)
-            .withMirroredValue("value", playerCountList, new String[] { "Player1", "Player2" })
+            .fork("hide_players")
+            .withMirroredValue("enabled", hideEnabled, true)
+            .withMirroredValue("value", playerList, new String[] { "Bot" })
             .finishBranch()
             .build();
 
